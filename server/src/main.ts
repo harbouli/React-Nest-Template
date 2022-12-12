e62258abea7c5a0b047f1611cf7e12cabf190c5a
harbouli <mharbouli@icloud.com>
2022-12-12 14:38:12 +0100

    Main Server Update

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,17 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const logger = new Logger('Main (main.ts)');
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        'http://localhost:8080',
-        /^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):8080$/,
-      ],
-    },
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors({ origin: ['http://localhost:3000'], credentials: true });
 
   const configService = app.get(ConfigService);
   const port = parseInt(configService.get('PORT'));
